070b9287c969ef36f4eb2529733711120bedebd9
Courtney Bracefield <courtneybracefield86@gmail.com>
2021-01-15 16:37:24 +1300

    Add model for a Blog Post.

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+SMALL = 100
+
 # Create your models here.
+class Post(models.Model):
+    """Model for blog post in databse."""
+
+    slug = models.CharField(max_length=SMALL, unique=True)
+    title = models.CharField(max_length=SMALL)
+    author = models.CharField(max_length=SMALL)
+    content = models.TextField()
